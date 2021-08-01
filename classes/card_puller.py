73c45c818bded4a6eaf64ac9b65671666798db1a
Colin S. Barquist <csbarquist@gmail.com>
2021-07-31 23:00:00 -0700

    card_puller updated
    card_puller.build_card_list() will now paginate through all of the pages and grab
    every card offered up by the /cards endpoint. Use card_puller.card_list to get the
    list of cards just retreived.

--- classes/card_puller.py
+++ classes/card_puller.py
@@ -9,17 +9,18 @@ class card_puller:
     provided by https://api.magicthegathering.io/v1/<item>.
     '''
 
-    def __inti__(self, base_url: str = 'https://api.magicthegathering.io/v1/cards') -> None:
+    def __init__(self, base_url: str = 'https://api.magicthegathering.io/v1/cards') -> None:
         '''
         :param url: URL for the domain serving the apis 
         :type url: str
         :returns: none
         '''
 
-        self._base_url = url
-        self.url = url
+        self._base_url = base_url
+        self._pointer_dict = {}
+        self._card_list = []
 
-    def get(self, url) -> Dict[str, Dict[str, Any]]:
+    def get(self, url) -> None:
         response = requests.get(url)
         
         # Pick out the d
@@ -27,15 +28,50 @@ class card_puller:
 
         # The get request returns a dict with only one entry 'cards'. This key
         # contains a list of the cards returned for the given page.
-        self.card_list = response.json()['cards']
+        self.card_page = response.json()['cards']
+        
+        # use get_pgaination_pointers to get update the pointers based on the 
+        # response
+        self.get_pagination_pointers()
 
-        return self.headers, self.card_list
+        return None
 
-    def pagination_pointers(headers: Dict[str, str]) -> Dict[str, str]:
+    def get_base_page(self) -> None:
+        self.get(self.base_url)
+
+    def get_next_page(self) -> None:
+       
+        # If there is a next url then get the cards and return true.
+        if 'next' in self.pointer_dict.keys():
+            self.get(self.pointer_dict['next'])
+            return True
+
+        return False
+        
+    def build_card_list(self) -> None:
+        '''
+        Builds card list starting at base_url. The function will paginate through
+        until the end of the pages. Each page will be appended onto the list of
+        cards.
+        '''
+        # Get the base page
+        self.get_base_page()
+        
+        # Begin card list with the base page
+        card_list = self.card_page
+
+        # get_next_page is true when a page was gotten. False at end of pages.
+        while self.get_next_page():
+            print('in while')
+            card_list.extend(self.card_page)
+
+        self._card_list = card_list
+        return None
+
+
+    def get_pagination_pointers(self) -> Dict[str, str]:
         '''
         Take in the headers and parse the response for the pagination pointers
-        :param headers: Dictionary containing the headers key/values
-        :type headers: Dict[str, str]
         :returns: Return a dictionary of pointers for the pagination. Pointer 
         labels are first, last, prev, next.
         '''
@@ -43,7 +79,7 @@ class card_puller:
         # The list of pointers is contained the the 'link' key and is a long 
         # string where different pointer are separated by ','. It also contains
         # some unnecessary whitespace. Strip whitespace and split on ','.
-        pointer_list = headers['link'].replace(' ', '').split(',')
+        pointer_list = self.headers['link'].replace(' ', '').split(',')
 
         # Initialize dictionary to store pointers after parsing
         pointer_dict = {}
@@ -61,24 +97,23 @@ class card_puller:
 
             pointer_dict[pointer_name] = pointer_url
 
+        self._pointer_dict = pointer_dict
+
         return pointer_dict
 
-            
+    ##############
+    # Properties #
+    ##############
 
-        ##############
-        # Properties #
-        ##############
+    @property
+    def base_url(self) -> str:
+        return self._base_url
 
-        @property
-        def base_url(self) -> str:
-            return self._base_url
+    @property
+    def pointer_dict(self) -> Dict:
+        return self._pointer_dict
 
-        @property
-        def url(self) -> str:
-            return self._url
+    @property
+    def card_list(self) -> List[Dict]:
+        return self._card_list
 
-        @url.setter
-        def url(self, url) -> None:
-            assert type(url) == str, 'url must be type string'
-
-            self._url = url
